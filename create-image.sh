09cf13eb815337d27c4d3c5706a33244211bd3f1
Axel Bauer <axel.bauer@bibliothek.uni-halle.de>
2021-04-01 17:20:39 +0200

    fix mistake

--- create-image.sh
+++ create-image.sh
@@ -10,8 +10,8 @@ echo start with DB: $DB_NAME, $DB_USER, DB_PASS
 
 VERSION=main
 PHP_TAIL=/alpine/apache/php
-# OJS_GIT=https://github.com/pkp/docker-ojs.git
-OJS_ULB=git@git.itz.uni-halle.de:ulb/ulb-ojs.git
+OJS_GIT=https://github.com/pkp/docker-ojs.git
+# OJS_ULB=git@git.itz.uni-halle.de:ulb/ulb-ojs.git
 
 
 git clone ${OJS_GIT} || echo "'${OJS_GIT}' just here"
