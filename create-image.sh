b9720e2398c25f36926c7f483a34f773a09a190e
Axel Bauer <axel.bauer@bibliothek.uni-halle.de>
2021-04-12 14:36:01 +0200

    copy files from source to home

--- create-image.sh
+++ create-image.sh
@@ -6,7 +6,7 @@ DB_NAME=$1
 DB_USER=$2
 DB_PASS=$3
 
-echo start with DB: $DB_NAME, $DB_USER, DB_PASS 
+echo start with DB: $DB_NAME, $DB_USER, DB_PASS
 
 VERSION=main
 PHP_TAIL=/alpine/apache/php
@@ -27,10 +27,17 @@ mkdir -p volumes/public
 
 OJS_HOME=$(find ./docker-ojs -type d -name ${VERSION})
 OJS_HOME=$OJS_HOME$PHP_TAIL
-if [ -z "$OJS_HOME$" ] ; then echo "**OJS Version $VERSION not found!**"; 
+if [ -z "$OJS_HOME$" ] ; then echo "**OJS Version $VERSION not found!**";
     else echo "Version '$VERSION' found --> $OJS_HOME"; fi
 
+echo copy $OJS_HOME/Dockerfile .
+cp $OJS_HOME/Dockerfile .
+
+echo copy $OJS_HOME/exclude.list
+cp $OJS_HOME/exclude.list .
+
+
+echo try start docker-compose with docker-compose-ulb.yml
 #start OJS
 
 docker-compose --file ./docker-compose-ulb.yml up
-                  
