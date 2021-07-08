fcb4772a1d323813cf16730f6b3e29eb006fa97a
Axel Bauer <axel.bauer@bibliothek.uni-halle.de>
2021-07-08 14:54:46 +0200

    add shebang

--- upgrade_ojs.sh
+++ upgrade_ojs.sh
@@ -1,25 +1,27 @@
+#!/bin/bash
+
 set -eu
 
 echo "upgrade ojs from version 3.1.1.2 up to 3_3_0-6"
 
 echo "drop database ojs"
 
-echo  "drop database ojs;" | docker exec -i ojs_db_ulb mysql -pXXX
+echo  "drop database ojs;" | docker exec -i ojs_db_ulb mysql -p$1
 
 echo "create database ojs"
 
-echo  "create database ojs;" | docker exec -i ojs_db_ulb mysql -pXXX
+echo  "create database ojs;" | docker exec -i ojs_db_ulb mysql -p$1
 
 echo "copy public files" | cp -aur /srv/ojs/public/ /data/ojs/
 echo "copy journals files" | cp -aur /home/ojs/journals/ /data/ojs/private/
-echo "copy userStats files" | cp -aur /home/ojs/userStats/ /data/ojs/private/
+echo "copy usageStats files" | cp -aur /home/ojs/usageStats/ /data/ojs/private/
 echo "copy scheduledTaskLogs files" | cp -aur /home/ojs/scheduledTaskLogs /data/ojs/private/
 
 echo "dump current ojs database" | sudo mysqldump ojs > /data/ojs/sqldumps/ojs.sql
 
 echo "recreate last ojs db form version 3.1.1.2 dump"
 
-cat /data/ojs/sqldumps/ojs.sql | docker exec -i ojs_db_ulb mysql -pXXX ojs
+cat /data/ojs/sqldumps/ojs.sql | docker exec -i ojs_db_ulb mysql -p$1 ojs
 
 OJS_VERSION=3_1_1-2
 
