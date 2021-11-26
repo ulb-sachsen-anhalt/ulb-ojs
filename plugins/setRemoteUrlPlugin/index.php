4c246324980049731de7d7f16f98dd37ee978ddf
Axel Bauer <axel.bauer@bibliothek.uni-halle.de>
2021-11-26 10:35:55 +0100

    initial commit

---
+++ plugins/setRemoteUrlPlugin/index.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ *
+ * Copyright (c) 2021 Universitäts- und Landesbibliothek Sachsen-Anhalt
+ * Distributed under the GNU GPL v2. For full terms see the file LICENCE.
+ *
+ */
+require_once('SetRemoteUrlPlugin.inc.php');
+
+return new SetRemoteUrlPlugin();
+?> 
